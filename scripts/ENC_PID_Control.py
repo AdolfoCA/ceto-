f5bcdb5d2ddf677b3716031a24f205dbeb6694f9
AdolfoCA <adocafaro@gmail.com>
2025-03-12 10:20:18 +0000

    adjust a few things

--- scripts/ENC_PID_Control.py
+++ scripts/ENC_PID_Control.py
@@ -12,7 +12,12 @@ from thruster_manager_v2 import ThrusterManager
 from create_new_csv_file import create_new_csv_file
 from geometry_msgs.msg import PoseStamped
 from guidance import Guidance
+from phe import paillier # for Paillier cryptography 
 
+
+
+global P, S 
+P,S = paillier.generate_paillier_keypair()
 
 ############################### PID Class #################################
 class PID:
@@ -21,10 +26,11 @@ class PID:
         self.kp = kp
         self.ki = ki
         self.kd = kd
-        self.setpoint = setpoint
+        self.integral_limit = integral_limit  # Limit for integral term
+
+        self.setpoint = setpoint 
         self.prev_error = 0
-        self.integral = 0
-        self.integral_limit = integral_limit  
+        self.integral = 0 
 
     def update(self, measurement,setpoint=None):
         """Calculate the PID control output."""
@@ -35,9 +41,9 @@ class PID:
         self.integral += error
         self.integral = np.clip(self.integral, -self.integral_limit, self.integral_limit)  
         derivative = error - self.prev_error
-        self.prev_error = error#!/usr/bin/env python3
+        self.prev_error = error
 
-############################### ROV PID Controller #################################
+############################ ROV_PID_Controller Class ########################################
 class ROVPIDController:
     """PID-based controller for an ROV using ROS."""
     def __init__(self):
@@ -95,13 +101,13 @@ class ROVPIDController:
 
         ## Now that i have checked the equilibrium for the roll and pitch --> set that as setpoint in the PID 
         # Initialize PID controllers 
-        self.pid_yaw = PID(kp=0.03, ki=0.01, kd=0.04, setpoint=170)
-        self.pid_z = PID(kp=4.0, ki=0.4, kd=1.8, setpoint=17.1)
-        self.pid_x = PID(kp=12.0, ki=0.004, kd=0.3, setpoint=0)
-        self.pid_y = PID(kp=2.45, ki=0.005, kd=0.008, setpoint=0)
+        self.pid_yaw = PID(kp=0.03, ki=0.01, kd=0.04, setpoint=P.encrypt(170))
+        self.pid_z = PID(kp=4.0, ki=0.4, kd=1.8, setpoint=P.encrypt(17.1))
+        self.pid_x = PID(kp=12.0, ki=0.004, kd=0.3, setpoint=P.encrypt(0))
+        self.pid_y = PID(kp=2.45, ki=0.005, kd=0.008, setpoint=P.encrypt(0))
 
-        self.pid_roll = PID(kp=0.02, ki=0.01, kd=0.18, setpoint=0)  
-        self.pid_pitch = PID(kp=0.046, ki=0.01, kd=0.14, setpoint=0)
+        self.pid_roll = PID(kp=0.02, ki=0.01, kd=0.18, setpoint=P.encrypt(0))  
+        self.pid_pitch = PID(kp=0.046, ki=0.01, kd=0.14, setpoint=P.encrypt(0))
 
         # CSV Logging
         self.csv_file = create_new_csv_file()
@@ -116,15 +122,22 @@ class ROVPIDController:
 
 
 ##################################### callback functions ########################################
+### Here I need to encrypte the messages I get from the sensors. 
 
     def imu_callback(self, msg):
         """Update roll, pitch, and yaw values from IMU data."""
         qx, qy, qz, qw = msg.orientation.x, msg.orientation.y, msg.orientation.z, msg.orientation.w
         self.roll, self.pitch, self.yaw = self.quaternion_to_euler(qx, qy, qz, qw)
+        ## Encypted the messages
+        self.safe_roll = P.encrypt(self.roll)
+        self.safe_pitch = P.encrypt(self.pitch)
+        self.safe_yaw = P.encrypt(self.yaw)
 
     def altitude_callback(self, msg):
         """Update altitude value."""
         self.altitude = msg.data
+        ## Encypted the messages
+        self.safe_altitude = P.encrypt(self.altitude)
 
     def pose_callback(self, msg):
         """Update x and y values from the Optitrack."""
@@ -132,6 +145,9 @@ class ROVPIDController:
         self.x = - msg.pose.position.x
         self.y = msg.pose.position.z 
         self.time_stamp = msg.header.stamp
+        ## Encypted the messages
+        self.safe_x = P.encrypt(self.x)
+        self.safe_y = P.encrypt(self.y)
         rospy.loginfo("New Pose received: x={:.3f}, y={:.3f}".format(self.x, self.y))
 
 
@@ -258,14 +274,30 @@ class ROVPIDController:
             self.set_pitch = self.pitch0
             self.set_yaw = self.yaw0
 
-            # Compute PID corrections
-            Mx = self.pid_roll.update(self.roll,self.set_roll)
-            My = self.pid_pitch.update(self.pitch,self.set_pitch)
-            Mz = self.pid_yaw.update(self.yaw,self.set_yaw)
-            Fz = self.pid_z.update(self.altitude,self.set_altitude)
-            Fx = self.pid_x.update(self.x,self.set_x)
-            Fy = self.pid_y.update(self.y,self.set_y)
+            ## Encytped the desireder location
+            self.safe_set_x = P.encrypt(self.set_x)
+            self.safe_set_y = P.encrypt(self.set_y)
+            self.safe_set_altitude = P.encrypt(self.set_altitude)
+            self.safe_set_roll = P.encrypt(self.set_roll)
+            self.safe_set_pitch = P.encrypt(self.set_pitch)
+            self.safe_set_yaw = P.encrypt(self.set_yaw)
+
+            # Compute Enc_PID corrections
+            Mx_safe = self.pid_roll.update(self.safe_roll,self.set_safe_roll)
+            My_safe = self.pid_pitch.update(self.safe_pitch,self.set_safe_pitch)
+            Mz_safe = self.pid_yaw.update(self.safe_yaw,self.set_safe_yaw)
+            Fz_safe = self.pid_z.update(self.safe_altitude,self.set_safe_altitude)
+            Fx_safe = self.pid_x.update(self.safe_x,self.set_safe_x)
+            Fy_safe = self.pid_y.update(self.safe_y,self.set_safe_y)
             #rospy.loginfo(f"Current Orientation: Roll={self.roll:.2f}°, Pitch={self.pitch:.2f}°, Yaw={self.yaw:.2f}°, altitude={self.altitude:.2f}m,  Pose: x={self.x.3f}, y={self.y.3f}")
+
+            ################### Decryption of the messages from the controller ############################
+            Fx = S.decrypt(Fx_safe)
+            Fy = S.decrypt(Fy_safe)
+            Fz = S.decrypt(Fz_safe)
+            Mx = S.decrypt(Mx_safe)
+            My = S.decrypt(My_safe)
+            Mz = S.decrypt(Mz_safe)
 
             # Log current sensor data
             rospy.loginfo("IMU Data: Roll={:.2f}°, Pitch={:.2f}°, Yaw={:.2f}°".format(self.roll, self.pitch, self.yaw))
